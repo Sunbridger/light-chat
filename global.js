1bcab5c0a092ddaaa315902fec1700bf724e1d27
Sunbridger <739272884@qq.com>
2019-08-25 17:53:24 +0800

    添加format

--- global.js
+++ global.js
@@ -1,3 +1,3 @@
-const service = 'http://129.204.236.232/';
+const service = 'http://129.204.236.232/'; //腾讯云服务器 9 25到期
 // const service = 'http://192.168.249.100:3000/';
 export default  service;
--- src/views/chat.vue
+++ src/views/chat.vue
@@ -119,6 +119,18 @@ export default {
             
         }
     },
+    filters: {
+        format(time) {
+            let gett = Date.parse(time);
+            let date = new Date(gett);
+            let Y = date.getFullYear() + '/';
+            let M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '/';
+            let D = date.getDate() + ' ';
+            let h = date.getHours() + ':';
+            let m = date.getMinutes();
+            return Y + M + D + h + m;
+        }
+    }
     
 };
 </script>
