6a210188f5f7d78a742227b0f2d81b48fb9838e9
Sunbridger <739272884@qq.com>
2019-11-19 14:33:33 +0800

    加入注释

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,16 @@ const resolve = dir => path.join(__dirname, dir);
 
 // const target = 'https://2261295nz5.51mypc.cn/';
 const target = 'http://129.204.236.232/';
+
+
+/**
+ *
+ * devServer.proxy 简介
+先简单介绍一下 devServer.proxy 的用法：
+注： 这里的 devServer.proxy 就是 vue-cli 中位于 /config/index.js 的 dev.proxyTable，后面不再赘述
+ *
+ *
+ */
 const publicPath = './';
 module.exports = {
   publicPath,
@@ -19,7 +29,7 @@ module.exports = {
       },
     },
   },
-  
+
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
       config.mode = 'production';
@@ -31,7 +41,7 @@ module.exports = {
       '@': resolve('./src'),
       'view': resolve('./src/views'),
       'api': resolve('./src/api'),
-      'assets': resolve('./src/assets') 
+      'assets': resolve('./src/assets')
     };
   },
 };
